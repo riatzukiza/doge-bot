5ee788eab5a23889109f58752b23ba93b0ec1d7c
marcinaj <andrew.marciniak@gmail.com>
2021-01-30 19:20:49 -0500

    removed useless file

--- src/data/.src.template.mjs
+++
@@ -1,3 +0,0 @@
-
-const cfg    = JSON.parse( await fs.readFile( "../cfg/cfg.src.SRC_NAME.json", "utf8" ) ); // edit
-const ignore = JSON.parse( await fs.readFile( "../cfg/cfg.src.ignore.json"  , "utf8" ) );
